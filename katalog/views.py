883e1c9d3f7b8bf5c18ce41753e7b67397a99e5a
ferdar7 <ferdar7@gmail.com>
2022-09-10 13:28:32 +0700

    first commit

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,15 @@
+from multiprocessing import context
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(req):
+    data_katalog = CatalogItem.objects.all()
+    ctx = {
+        'list_katalog':  data_katalog,
+        'nama': 'Muhammad Raditya Hanif',
+        'id': '2106750585'
+
+    }
+
+    return render(req, "katalog.html", ctx)
