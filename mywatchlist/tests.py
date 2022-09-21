0912e6a41594300c4ae0ec19703a788f3a495966
ferdar7 <ferdar7@gmail.com>
2022-09-22 00:21:15 +0700

    fixed procfile

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.test import Client
 
 # Create your tests here.
+class WatchlistTest(TestCase):
+
+    def test_mywatchlist(self):
+        response  = Client().get('/mywatchlist/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_mywatchlist_html(self):
+        response  = Client().get('/mywatchlist/html/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_mywatchlist_json(self):
+        response  = Client().get('/mywatchlist/json/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_mywatchlist_xml(self):
+        response  = Client().get('/mywatchlist/xml/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_correct_template(self):
+        response = self.client.get('/mywatchlist/')
+        self.assertTemplateUsed(response, 'watchlist.html')
